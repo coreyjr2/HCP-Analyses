4a9c238b84beba6d4a4878aae333ae5db9ac0e63
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-03-30 16:19:49 -0500

    tSNE changes

--- tSNE_cluster.py
+++ tSNE_cluster.py
@@ -16,16 +16,18 @@ try:
   import json 
   import paramiko
   from scp import SCPClient
-  import getpass
+  # import getpass
   import numpy as np
   from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
-  import pickle as pk
-  import seaborn as sb
+  # import matplotlib.pyplot as plt
+  # import pickle as pk
+  # import seaborn as sb
   import time
 except Exception as e:
   print(f'Error loading libraries: ')
   raise Exception(e)
+
+
 
 
 # Global Variables
@@ -35,29 +37,29 @@ try:
   source_path = os.path.dirname(os.path.abspath(__file__)) + sep
   sys_name = platform.system() 
   hostname = platform.node()
-  output_path = '/mnt/usb1/hcp_analysis_output/'
-  local_path = '/mnt/usb1/hcp_analysis_output/'
+  output_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
+  local_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
   # output_path = 'C:\\Users\\Sarah Melissa\\Documents\\output\\'
   # local_path = 'C:\\Users\\Sarah Melissa\\Documents\\temp\\'
   run_uid = '8d2513'
-  remote_outpath = '/mnt/usb1/hcp_analysis_output/'
-  uname = 'solshan2'
-  datahost = 'r2.psych.uiuc.edu'
-  source_path = '/mnt/usb1/hcp_analysis_output/'
+  remote_outpath = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
+  # uname = 'solshan2'
+  # datahost = 'r2.psych.uiuc.edu'
+  source_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
 except:
   pass
 
 # Template Functions
-try:
-  def createSSHClient(server, user, password, port=22):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    return client
-except Exception as e:
-  print(f'Error defining template functions: ')
-  raise Exception(e)
+# try:
+#   def createSSHClient(server, user, password, port=22):
+#     client = paramiko.SSHClient()
+#     client.load_system_host_keys()
+#     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+#     client.connect(server, port, user, password)
+#     return client
+# except Exception as e:
+#   print(f'Error defining template functions: ')
+#   raise Exception(e)
 
 outpath = output_path + run_uid + sep
 tsne_out = outpath + 't-SNE' + sep
@@ -67,24 +69,6 @@ try:
 except:
     pass
 
-# # SCP data to temp location
-# #if source_path!=None:
-#   # Interupt request for password and username if none passed
-#   if uname == None:
-#     uname = getpass.getpass(f'Username for {datahost}:')
-#   psswd = getpass.getpass(f'Password for {uname}@{datahost}:')
-#   src_basepath = source_path
-#   download_start_time = dt.datetime.now()
-#   print('Starting Data Transfer: ', download_start_time)
-#   try:
-#     ssh = paramiko.SSHClient()
-#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#     ssh.connect(datahost, 22, uname, psswd)
-#     scp = SCPClient(ssh.get_transport())
-#     scp.get(source_path + run_uid, local_path + run_uid, recursive=True)
-#   except Exception as e:
-#     print(f'Error transferring data from {uname}@{datahost} ')
-#     raise Exception(e)
 
 # Start logging file
 total_start_time = dt.datetime.now()
@@ -192,35 +176,35 @@ parcel_connection_features = feature_set_dict[k]
 
 # Use to keep track of length of feature sets
 length_list = [
-  19900, #1
-  19884, #2
-  18540, #3
-  13981, #4
-  10027, #5
-  7482, #6
-  5847, #7
-  4638, #8
-  3771, #9
-  3149, #10
-  2679, #11
-  2297, #12
-  1980, #13
-  1725, #14
-  1533, #15
-  1354, #16
-  1205, #17
-  1078, #18
-  960, #19
-  876, #20
-  807, #21
-  734, #22
-  676, #23
-  622, #24
-  569, #25
-  522, #26
-  480, #27
-  443, #28
-  414 #29
+  19900,
+  19884,
+  18540,
+  13981,
+  10027,
+  7482,
+  5847,
+  4638,
+  3771,
+  3149,
+  2679,
+  2297,
+  1980,
+  1725,
+  1533,
+  1354,
+  1205,
+  1078,
+  960,
+  876,
+  807,
+  734,
+  676,
+  622,
+  569,
+  522,
+  480,
+  443,
+  414
 ]
 
 
@@ -234,7 +218,7 @@ numeric_task_ref = pd.DataFrame(
   }
 )
 plotting_df = pd.merge(plotting_df, numeric_task_ref, on='task')
-plotting_df.to_csv(f'{tsne_out}Plotting_DF.csv',index=False)
+
 # Create function to run tSNE and save plots
 def run_plt_tsne(data, label, perplexity = 77, learning_rate=50, init = 'random', verbose=1, n_iter=2000):
   start_time = dt.datetime.now()
@@ -327,20 +311,22 @@ try:
   # source_path = os.path.dirname(os.path.abspath(__file__)) + sep
   sys_name = platform.system() 
   hostname = platform.node()
-  # output_path = '/mnt/usb1/hcp_analysis_output/'
-  # local_path = '/mnt/usb1/hcp_analysis_output/'
+  # output_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
+  # local_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
   output_path = 'C:\\Users\\Sarah Melissa\\Documents\\output\\'
   local_path = 'C:\\Users\\Sarah Melissa\\Documents\\temp\\'
   run_uid = '8d2513'
-  remote_outpath = '/mnt/usb1/hcp_analysis_output/'
+  remote_outpath = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
   uname = 'solshan2'
   datahost = 'r2.psych.uiuc.edu'
-  source_path = '/mnt/usb1/hcp_analysis_output/'
+  source_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
 except:
   pass
 
 perp_list = [50, 55, 60, 70, 75, 80, 85, 90]
 transfer_list = []
+
+
 
 for perp in perp_list:
   # 1) PCA
@@ -378,7 +364,7 @@ if uname == None:
 psswd = getpass.getpass(f'Password for {uname}@{datahost}:')
 
 local_path = f'C:\\Users\\Sarah Melissa\\Documents\\output\\{run_uid}\\t-SNE\\'
-source_path = f'/mnt/usb1/hcp_analysis_output/{run_uid}/t-SNE/'
+source_path = f'/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/{run_uid}/t-SNE/'
 
 for image in transfer_list:
   tf = True
--- tSNE_cluster_v2.py
+++ tSNE_cluster_v2.py
@@ -5,6 +5,7 @@
 # Downloads data from R2 (on CANOPY CLuster)
 # Reads in data
 # Runs TSNE
+# Saves TSNE variables as output
 
 # Imports
 try:
@@ -14,50 +15,42 @@ try:
   import os
   import datetime as dt
   import json 
-  import paramiko
-  from scp import SCPClient
-  import getpass
+  # import paramiko
+  # from scp import SCPClient
+  # import getpass
   import numpy as np
   from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
+  # import matplotlib.pyplot as plt
   import pickle as pk
-  import seaborn as sb
+  # import seaborn as sb
   import time
 except Exception as e:
   print(f'Error loading libraries: ')
   raise Exception(e)
 
 
+
+
 # Global Variables
 try:
   sep = os.path.sep
   # source_path = '/home/kbaacke/HCP_Analyses/'
-  source_path = os.path.dirname(os.path.abspath(__file__)) + sep
+  source_path = '/data/hx-hx1/kbaacke/hcp_analysis_output/'
+  # source_path = os.path.dirname(os.path.abspath(__file__)) + sep
   sys_name = platform.system() 
   hostname = platform.node()
-  output_path = '/mnt/usb1/hcp_analysis_output/'
-  local_path = '/mnt/usb1/hcp_analysis_output/'
+  output_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
+  local_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
   # output_path = 'C:\\Users\\Sarah Melissa\\Documents\\output\\'
   # local_path = 'C:\\Users\\Sarah Melissa\\Documents\\temp\\'
   run_uid = '8d2513'
-  remote_outpath = '/mnt/usb1/hcp_analysis_output/'
-  uname = 'solshan2'
-  datahost = 'r2.psych.uiuc.edu'
-  source_path = '/mnt/usb1/hcp_analysis_output/'
-except:
-  pass
-
-# Template Functions
-try:
-  def createSSHClient(server, user, password, port=22):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    return client
+  remote_outpath = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
+  # uname = 'solshan2'
+  # datahost = 'r2.psych.uiuc.edu'
+  source_path = '/data/hx-hx1/kbaacke/datasets/hcp_analysis_output/'
 except Exception as e:
-  print(f'Error defining template functions: ')
-  raise Exception(e)
+  print(e)
+
 
 outpath = output_path + run_uid + sep
 tsne_out = outpath + 't-SNE' + sep
@@ -67,24 +60,6 @@ try:
 except:
     pass
 
-# # SCP data to temp location
-# #if source_path!=None:
-#   # Interupt request for password and username if none passed
-#   if uname == None:
-#     uname = getpass.getpass(f'Username for {datahost}:')
-#   psswd = getpass.getpass(f'Password for {uname}@{datahost}:')
-#   src_basepath = source_path
-#   download_start_time = dt.datetime.now()
-#   print('Starting Data Transfer: ', download_start_time)
-#   try:
-#     ssh = paramiko.SSHClient()
-#     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-#     ssh.connect(datahost, 22, uname, psswd)
-#     scp = SCPClient(ssh.get_transport())
-#     scp.get(source_path + run_uid, local_path + run_uid, recursive=True)
-#   except Exception as e:
-#     print(f'Error transferring data from {uname}@{datahost} ')
-#     raise Exception(e)
 
 # Start logging file
 total_start_time = dt.datetime.now()
@@ -192,35 +167,35 @@ parcel_connection_features = feature_set_dict[k]
 
 # Use to keep track of length of feature sets
 length_list = [
-  19900, #1
-  19884, #2
-  18540, #3
-  13981, #4
-  10027, #5
-  7482, #6
-  5847, #7
-  4638, #8
-  3771, #9
-  3149, #10
-  2679, #11
-  2297, #12
-  1980, #13
-  1725, #14
-  1533, #15
-  1354, #16
-  1205, #17
-  1078, #18
-  960, #19
-  876, #20
-  807, #21
-  734, #22
-  676, #23
-  622, #24
-  569, #25
-  522, #26
-  480, #27
-  443, #28
-  414 #29
+  19900,
+  19884,
+  18540,
+  13981,
+  10027,
+  7482,
+  5847,
+  4638,
+  3771,
+  3149,
+  2679,
+  2297,
+  1980,
+  1725,
+  1533,
+  1354,
+  1205,
+  1078,
+  960,
+  876,
+  807,
+  734,
+  676,
+  622,
+  569,
+  522,
+  480,
+  443,
+  414
 ]
 
 
@@ -234,7 +209,30 @@ numeric_task_ref = pd.DataFrame(
   }
 )
 plotting_df = pd.merge(plotting_df, numeric_task_ref, on='task')
-plotting_df.to_csv(f'{tsne_out}Plotting_DF.csv',index=False)
+
+# Create function to run tSNE
+def run_tsne(data, label, perplexity = 77, learning_rate=50, init = 'random', verbose=1, n_iter=2000, n_jobs = 1):
+  start_time = dt.datetime.now()
+  # Run tSNE
+  try:
+    tSNE_output = TSNE(
+      n_components=2,
+      perplexity=perplexity,
+      learning_rate=learning_rate,
+      init=init,
+      verbose=verbose,
+      n_iter=n_iter,
+      n_jobs = n_jobs
+      ).fit_transform(data)
+    np.save(f'{tsne_out}{label}_tSNE.npy',tSNE_output)
+    # sb.scatterplot(tSNE_output[:,0], tSNE_output[:,1], hue = plotting_df['label'], s=3).set(title=label)
+    # plt.savefig(f'{tsne_out}{label} tSNE.png', transparent=True)
+    end_time = dt.datetime.now()
+    runtime = end_time - start_time
+    print(f'tSNE on {label} complete. Runtime: {runtime}')
+  except Exception as e:
+    print(f'Error running tSNE for {label}: {e}')
+
 # Create function to run tSNE and save plots
 def run_plt_tsne(data, label, perplexity = 77, learning_rate=50, init = 'random', verbose=1, n_iter=2000):
   start_time = dt.datetime.now()
@@ -258,106 +256,58 @@ def run_plt_tsne(data, label, perplexity = 77, learning_rate=50, init = 'random'
 
 #### Run to here to set-up ####
 
-perp_list = [50, 55, 60, 70, 75, 80, 85, 90]
+perp_list = [
+  50, 
+  # 55, 
+  # 60, 
+  # 70, 
+  75, 
+  # 80, 
+  # 85, 
+  90
+]
 
+full_data = feature_set_dict[k]['train_x']
 for perp in perp_list:
   # The following sections will perform tSNE on the 4 different feature selection sets we have
   # 1) PCA
-  run_plt_tsne(
+  run_tsne(
     data = np.array(parcel_connection_features['train_pca']),
-    label = 'PCA',
-    perplexity = perp
+    label = f'PCA_perp{perp}',
+    perplexity = perp,
+    n_jobs = 4
   )
-  # # 2) Hierarchical selected features
-  for level in list(feature_set_dict[k]['hierarchical_selected_features'].keys())[:]:
-    feature_index = feature_set_dict[k]['hierarchical_selected_features'][level]
-    feature_len = len(feature_index)
-    run_plt_tsne(
-      data = np.array(feature_set_dict[k]['train_x'][feature_set_dict[k]['train_x'].columns[feature_index]]),
-      label = f'HFS level {level} {feature_len} features'
-    )
-  # 3) Random Forest Select from Model
-  for level in list(feature_set_dict[k]['hierarchical_selected_features'].keys())[:]:
-    feature_index = feature_set_dict[k][f'rf_selected_{x}']
-    feature_len = len(feature_index)
-    run_plt_tsne(
-      data = np.array(feature_set_dict[k]['train_x'][feature_index]),
-      label = f'RF Selected from Model {feature_len} features'
-    )
-  # 4) Permutation Importance features
-  n_estimators = 500
-  n_repeats = 50
-  perm_importances = feature_set_dict[k][f'feature_importances_{n_estimators}']
-  ranked_indices = np.argsort(perm_importances)[::-1]
-  for n in length_list[1:]: # Starting this at 1 so you don't run this on the full dataset (at position 1), 29 options
-    feature_index = ranked_indices[:n]
-    run_plt_tsne(
-      data = np.array(feature_set_dict[k]['train_x'][feature_set_dict[k]['train_x'].columns[feature_index]]),
-      label = f'RF Permutation Importance {n} features'
-    )
-
-# Script to periodically try to pull output image files from the cluster (run LOCAL)
-
-# Imports
-try:
-  import platform
-  import logging
-  import pandas as pd
-  import os
-  import datetime as dt
-  import json 
-  import paramiko
-  from scp import SCPClient
-  import getpass
-  import numpy as np
-  from sklearn.manifold import TSNE
-  import matplotlib.pyplot as plt
-  import pickle as pk
-  import seaborn as sb
-  import time
-except Exception as e:
-  print(f'Error loading libraries: ')
-  raise Exception(e)
-
-
-# Global Variables
-try:
-  sep = os.path.sep
-  # source_path = '/home/kbaacke/HCP_Analyses/'
-  # source_path = os.path.dirname(os.path.abspath(__file__)) + sep
-  sys_name = platform.system() 
-  hostname = platform.node()
-  # output_path = '/mnt/usb1/hcp_analysis_output/'
-  # local_path = '/mnt/usb1/hcp_analysis_output/'
-  output_path = 'C:\\Users\\Sarah Melissa\\Documents\\output\\'
-  local_path = 'C:\\Users\\Sarah Melissa\\Documents\\temp\\'
-  run_uid = '8d2513'
-  remote_outpath = '/mnt/usb1/hcp_analysis_output/'
-  uname = 'solshan2'
-  datahost = 'r2.psych.uiuc.edu'
-  source_path = '/mnt/usb1/hcp_analysis_output/'
-except:
-  pass
-
-perp_list = [50, 55, 60, 70, 75, 80, 85, 90]
-transfer_list = []
 
 for perp in perp_list:
-  # 1) PCA
-  label = 'PCA'
-  transfer_list.append(f'{label} tSNE.png')
+  for n in length_list[6:]:
+    run_tsne(
+      data = parcel_connection_features['train_pca'][:, 0:n],
+      label = f'PCA_{n}_perp{perp}',
+      perplexity = perp,
+      n_jobs = 4
+    )
+
+for perp in perp_list:
   # # 2) Hierarchical selected features
   for level in list(feature_set_dict[k]['hierarchical_selected_features'].keys())[:]:
     feature_index = feature_set_dict[k]['hierarchical_selected_features'][level]
     feature_len = len(feature_index)
-    label = f'HFS level {level} {feature_len} features'
-    transfer_list.append(f'{label} tSNE.png')
+    run_tsne(
+      data = np.array(feature_set_dict[k]['train_x'][feature_set_dict[k]['train_x'].columns[feature_index]]),
+      label = f'HFS level {level} {feature_len} features_perp{perp}'
+    )
+
+for perp in perp_list:
   # 3) Random Forest Select from Model
   for level in list(feature_set_dict[k]['hierarchical_selected_features'].keys())[:]:
     feature_index = feature_set_dict[k][f'rf_selected_{x}']
     feature_len = len(feature_index)
-    label = f'RF Selected from Model {feature_len} features'
-    transfer_list.append(f'{label} tSNE.png')
+    run_tsne(
+      data = np.array(feature_set_dict[k]['train_x'][feature_index]),
+      label = f'RF Selected from Model {feature_len} features_perp{perp}'
+    )
+
+for perp in perp_list:
   # 4) Permutation Importance features
   n_estimators = 500
   n_repeats = 50
@@ -365,35 +315,8 @@ for perp in perp_list:
   ranked_indices = np.argsort(perm_importances)[::-1]
   for n in length_list[1:]: # Starting this at 1 so you don't run this on the full dataset (at position 1), 29 options
     feature_index = ranked_indices[:n]
-    label = f'RF Permutation Importance {n} features'
-    transfer_list.append(f'{label} tSNE.png')
+    run_tsne(
+      data = np.array(feature_set_dict[k]['train_x'][feature_set_dict[k]['train_x'].columns[feature_index]]),
+      label = f'RF Permutation Importance {n} features_perp{perp}'
+    )
 
-
-
-
-# SCP output from cluster to local directory
-
-if uname == None:
-  uname = getpass.getpass(f'Username for {datahost}:')
-psswd = getpass.getpass(f'Password for {uname}@{datahost}:')
-
-local_path = f'C:\\Users\\Sarah Melissa\\Documents\\output\\{run_uid}\\t-SNE\\'
-source_path = f'/mnt/usb1/hcp_analysis_output/{run_uid}/t-SNE/'
-
-for image in transfer_list:
-  tf = True
-  while tf:
-    download_start_time = dt.datetime.now()
-    print(f'Starting {image} transfer: ', download_start_time)
-    try:
-      ssh = paramiko.SSHClient()
-      ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-      ssh.connect(datahost, 22, uname, psswd)
-      scp = SCPClient(ssh.get_transport())
-      scp.get(source_path + run_uid, local_path + run_uid)
-      download_end_time = dt.datetime.now()
-      print(f'Starting {image} transfer complete: ', download_end_time)
-      tf = False
-    except Exception as e:
-      # print(f'Error transferring data from {uname}@{datahost} ')
-      time.sleep(20)
