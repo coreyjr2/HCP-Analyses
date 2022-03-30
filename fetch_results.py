97181fc0570f67937cefc3191f82c9aa832105a0
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-03-30 13:14:35 -0500

    script to fetch results

---
+++ fetch_results.py
@@ -0,0 +1,50 @@
+import os
+import shutil
+import datetime as dt
+import pandas as pd
+import paramiko
+from scp import SCPClient
+import zipfile
+import getpass
+username = 'kbaacke'
+hostname = 'r2.psych.uiuc.edu'
+
+def createSSHClient(server, port, user, password):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(server, port, user, password)
+    return client
+
+sep = os.path.sep
+remote_sep = '/'
+source_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + sep
+base = f'S:\\'
+remote_base = '/mnt/usb1'
+
+output_path = '{base}{sep}hcp_analysis_output{sep}'
+
+metapath = output_path + 'metadata{sep}'
+confpath =  output_path +  'confusion{sep}'
+classpath = output_path + 'classification{sep}'
+inputpath = output_path + 'inputs{sep}'
+weightpath = output_path + 'weights{sep}'
+accuracy_path = output_path + 'Prediction_Accuracies.csv'
+
+# psswd = getpass.getpass(f'Password for {username}@{hostname}:')
+start_time = dt.datetime.now()
+ssh = createSSHClient(f'{hostname}', 22, f'{username}', getpass.getpass(f'Password for {username}@{hostname}:'))
+# ssh = createSSHClient(f'{hostname}', 22, f'{username}', f'{psswd}')
+scp = SCPClient(ssh.get_transport())
+
+# scp.get(output_path.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
+
+# scp.get(metapath.format(base = remote_base, sep=remote_sep), metapath.format(base = base, sep=sep), recursive=True)
+# scp.get(confpath.format(base = remote_base, sep=remote_sep), confpath.format(base = base, sep=sep), recursive=True)
+# scp.get(classpath.format(base = remote_base, sep=remote_sep), classpath.format(base = base, sep=sep), recursive=True)
+
+scp.get(accuracy_path.format(base = remote_base, sep=remote_sep), accuracy_path.format(base = base, sep=sep))
+
+
+end_time = dt.datetime.now()
+print('Done. Runtime: ', end_time - start_time)
