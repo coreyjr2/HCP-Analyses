baad0925000b8cb389734caa70a8ae20f5ad57bb
kabaacke-psy <55969421+kabaacke-psy@users.noreply.github.com>
2022-05-05 15:59:56 -0500

    fetch script updated

--- fetch_results.py
+++ fetch_results.py
@@ -22,8 +22,8 @@ source_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + sep
 base = f'S:\\'
 remote_base = '/data/hx-hx1/kbaacke/datasets/'
 # remote_base = '/mnt/usb1/'
-# output_path = '{base}hcp_analysis_output{sep}8d2513{sep}'
-output_path = '{base}MSC-HCP_analysis{sep}'
+output_path = '{base}hcp_analysis_output{sep}89952a{sep}'
+# output_path = '{base}MSC-HCP_analysis{sep}'
 
 try:
   os.makedir(output_path.format(base = base, sep=sep))
@@ -33,9 +33,10 @@ except:
 metapath = output_path + 'metadata{sep}'
 confpath =  output_path +  'confusion{sep}'
 classpath = output_path + 'classification{sep}'
-inputpath = output_path + 'inputs{sep}'
+inputpath = output_path + 'FeatureSelection{sep}parcel_connection{sep}'
 weightpath = output_path + 'weights{sep}'
 tsnepath = output_path+ 't-SNE{sep}'
+metadatapath = output_path + '89952ametadata.json'
 accuracy_path = output_path + 'Prediction_Accuracies.csv'
 length_path = output_path + 'feature_length_index.csv'
 
@@ -47,12 +48,15 @@ scp = SCPClient(ssh.get_transport())
 
 # scp.get(output_path.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
 
-# scp.get(metapath.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
-# scp.get(confpath.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
-# scp.get(classpath.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
-# scp.get(tsnepath.format(base = remote_base, sep=remote_sep), output_path.format(base = base, sep=sep), recursive=True)
+scp.get(metapath.format(base = remote_base, sep=remote_sep), metapath.format(base = base, sep=sep), recursive=True)
+scp.get(confpath.format(base = remote_base, sep=remote_sep), confpath.format(base = base, sep=sep), recursive=True)
+scp.get(classpath.format(base = remote_base, sep=remote_sep), classpath.format(base = base, sep=sep), recursive=True)
+# scp.get(tsnepath.format(base = remote_base, sep=remote_sep), tsnepath.format(base = base, sep=sep), recursive=True)
 scp.get(accuracy_path.format(base = remote_base, sep=remote_sep), accuracy_path.format(base = base, sep=sep))
+# scp.get(metadatapath.format(base = remote_base, sep=remote_sep), metadatapath.format(base = base, sep=sep))
+
 # scp.get(length_path.format(base = remote_base, sep=remote_sep), length_path.format(base = base, sep=sep))
+# scp.get(inputpath.format(base = remote_base, sep=remote_sep), inputpath.format(base = base, sep=sep), recursive=True)
 
 end_time = dt.datetime.now()
 print('Done. Runtime: ', end_time - start_time)
